3195d2660b8a192ff5bb8ce4e85a891abcc75815
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-08-31 14:12:18 -0700

    🚨 Add example asuimg table is build (read file directly)

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import pandas as pd
-import descarga_datos as dd
-import json
-import requests
-import io
 
 
 st.write(
@@ -14,11 +10,11 @@ V3
 """
 )
 
-df = pd.read_csv("tests/data/estaciones_guadalupe.csv")
+df = pd.read_csv("/workdir/data/seabird_tracking/bl_gps_albatross_guadalupe.csv")
 
 st.download_button(
     label="Download data as CSV",
     data=df.to_csv(),
-    file_name='large_df.csv',
-    mime='text/csv',
+    file_name="large_df.csv",
+    mime="text/csv",
 )
