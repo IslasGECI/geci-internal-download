0282ead8efe7d398ea0475432961e772b51a7f80
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-08-31 12:58:47 -0700

    🚧 Add first example of file to download

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,5 +1,22 @@
 import streamlit as st
 import pandas as pd
+import descarga_datos as dd
+import json
+import requests
+import io
+
+
+TEXTO_ANALYSIS = """{
+    "source": "tabular_data_packages",
+    "path": "roedores_capturarecaptura_cedros",
+    "filename": "roedores_capturarecaptura_cedros.csv",
+    "version": "e511b813667ef3063c47b47c954d3b8c202ef709",
+    "type": "datapackage"
+}"""
+
+
+analisis: dict = json.loads(TEXTO_ANALYSIS)
+datafile = dd.internals.DataFile(**analisis)
 
 st.write(
     """
@@ -8,5 +25,13 @@ Hello world
 V3
 """
 )
-df = pd.read_csv("tests/data/estaciones_guadalupe.csv")
-st.line_chart(df, x="Time", y="Temp_Out")
+
+response = requests.get(datafile.get_url_to_file()).content
+df = pd.read_csv(io.StringIO(response.decode("utf-8")))
+
+st.download_button(
+    label="Download data as CSV",
+    data=df.to_csv(),
+    file_name='large_df.csv',
+    mime='text/csv',
+)
