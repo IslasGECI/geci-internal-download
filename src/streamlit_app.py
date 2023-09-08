a4fbf68884d4b0f6559b4b3d423b7ba3aa575d4c
Mario Villasante <mario.villasante@islas.org.mx>
2023-09-08 13:42:14 -0700

    🚀👽 Use api

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -5,19 +5,27 @@ import requests
 st.write(
     """
 # GECI
-Hello world
-V3
+Hola Fer. \n
+Aquí puedes descargar los datos de grabaciones vocalizaciones Socorro. \n
+Un saludo desde CD 💻
 """
 )
 
-r = requests.get("http://core_api:10000").json()
-st.write(r)
+data_request = requests.get("http://data_api:10000/api/v1/data/grabaciones_socorro").json()
+datapackage_request = requests.get(
+    "http://data_api:10000/api/v1/data/grabaciones_socorro_datapackage"
+).json()
 
-df = pd.read_csv("/workdir/data/seabird_tracking/bl_gps_albatross_guadalupe.csv")
+df = pd.read_json(data_request)
+
+st.write(datapackage_request["resources"][0]["description"])
 
 st.download_button(
     label="Download data as CSV",
     data=df.to_csv(),
-    file_name="large_df.csv",
+    file_name=f"{datapackage_request['resources'][0]['name']}.csv",
     mime="text/csv",
 )
+
+
+st.dataframe(df)
